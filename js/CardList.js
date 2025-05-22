99d780c8435258ba6ca03c3ee798849de9751e31
Aylin <aylin_santacruz@hotmail.com>
2025-05-22 02:37:12 -0500

    feat:add detail component

--- js/CardList.js
+++ js/CardList.js
@@ -1,6 +1,47 @@
-function CardComponent({ evento }) {
+function DetailEvent({ evento, onBack }) {
   return (
-    <div className="w-[300px] bg-slate-700 rounded-xl shadow-xl overflow-hidden flex-shrink-0 flex flex-col">
+    <div className="max-w-3xl mx-auto p-6 rounded-lg text-white">
+      <button
+        onClick={onBack}
+        className="mb-4 px-4 py-2 bg-blue-600 rounded hover:bg-blue-700"
+      >
+        <i class="fa-solid fa-arrow-left"></i>
+      </button>
+
+      <img
+        src={evento.imagen}
+        alt={evento.nombre}
+        className="w-full h-64 object-cover rounded"
+      />
+      <h2 className="text-3xl font-bold mt-4">{evento.nombre}</h2>
+      <p className="mt-2 text-gray-300">{evento.descripcion}</p>
+
+      <div className="mt-4 text-sm text-gray-400">
+        <p>
+          <strong>Lugar:</strong> {evento.lugar}
+        </p>
+        <p>
+          <strong>Categoría:</strong> {evento.categoria}
+        </p>
+        <p>
+          <strong>Fecha:</strong>{" "}
+          {new Date(evento.fecha).toLocaleDateString(undefined, {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </p>
+        <p>
+          <strong>Hora:</strong> {evento.hora} hrs
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function CardComponent({ evento, onEventClick }) {
+  return (
+    <div className="w-[300px] bg-slate-700 rounded-xl shadow-xl overflow-hidden flex-shrink-0 flex flex-col" onClick={() => onEventClick(evento)}>
       <img src={evento.imagen} alt={evento.nombre} className="w-full h-[200px] object-cover" />
 
       <div className="p-4 space-y-2 text-sm">
@@ -51,14 +92,14 @@ function CardComponent({ evento }) {
   );
 }
 
-function ListComponent({ data }) {
+function ListComponent({ data,onEventClick  }) {
   return (
     <section className="relative py-12 px-4 text-white">
       {/* Contenido */}
       <div className="relative z-10 max-w-7xl mx-auto">
         <div className="flex flex-wrap justify-center gap-6">
           {data.map((evento, index) => (
-            <CardComponent key={index} evento={evento} />
+            <CardComponent key={index} evento={evento} onEventClick ={onEventClick}/>
           ))}
         </div>
       </div>
@@ -68,13 +109,31 @@ function ListComponent({ data }) {
 
 function CardList() {
   const [data, setData] = React.useState([]);
-
+  const [selectedEvent, setSelectedEvent] = React.useState(null);
   React.useEffect(() => {
     fetch("../data/data.json")
       .then((res) => res.json())
       .then((data) => setData(data))
       .catch((err) => console.error("Error cargando datos:", err));
   }, []);
-   return <ListComponent data={data} />;
+   const handleEventClick = (evento) => {
+    setSelectedEvent(evento);
+  };
+
+  const handleBack = () => {
+    setSelectedEvent(null);
+  };
+
+   if (selectedEvent) {
+    return <DetailEvent evento={selectedEvent} onBack={handleBack} />;
+  }
+   return(
+     <section className="py-10 px-4 text-white">
+      <h2 className="text-3xl font-bold text-blue-200 mb-2 text-center">Eventos</h2>
+      <div className="w-32 h-1 bg-blue-200 mx-auto mb-8 rounded"></div>
+
+      <ListComponent data={data} onEventClick={handleEventClick} />
+    </section>
+   )
 }
 ReactDOM.render(<CardList />, document.getElementById("card"));
